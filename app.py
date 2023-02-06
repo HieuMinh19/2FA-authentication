d53077f5f6888ec6d4dc248471cae78cce85fb1d
Felix <felix@Hieus-MacBook-Pro.local>
2023-02-06 23:54:24 +0700

    complete build code

--- app.py
+++ app.py
@@ -8,12 +8,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "APP_SECRET_KEY"
 Bootstrap(app)
 
-
-
 # homepage route
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Welcome to Type For Life Channel!</h1>"
 
 # login page route
 @app.route("/login/")
@@ -23,46 +21,38 @@ def login():
 # login form route
 @app.route("/login/", methods=["POST"])
 def login_form():
-    # demo creds
-    creds = {"username": "test", "password": "password"}
+    demoCreds = {"username": "felix", "password": "password"}
 
     # getting form data
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # authenticating submitted creds with demo creds
-    if username == creds["username"] and password == creds["password"]:
-        # inform users if creds are valid
+    # authenticating submitted demoCreds with demo demoCreds
+    if username == demoCreds["username"] and password == demoCreds["password"]:
         flash("The credentials provided are valid", "success")
-        # return redirect(url_for("login"))
         return redirect(url_for("login_2fa"))
     else:
-        # inform users if creds are invalid
-        flash("You have supplied invalid login credentials!", "danger")
+        flash("Invalid login credentials!", "danger")
         return redirect(url_for("login"))
 
 # 2FA page route
 @app.route("/login/2fa/")
 def login_2fa():
-    # generating random secret key for authentication
     secret = pyotp.random_base32()
     return render_template("login_2fa.html", secret=secret)
 
-# 2FA form route
 @app.route("/login/2fa/", methods=["POST"])
 def login_2fa_form():
-    # getting secret key used by user
+
     secret = request.form.get("secret")
-    # getting OTP provided by user
     otp = int(request.form.get("otp"))
 
     # verifying submitted OTP with PyOTP
     if pyotp.TOTP(secret).verify(otp):
-        # inform users if OTP is valid
         flash("The TOTP 2FA token is valid", "success")
         return redirect(url_for("login_2fa"))
     else:
-        # inform users if OTP is invalid
+        # OTP is invalid
         flash("You have supplied an invalid 2FA token!", "danger")
         return redirect(url_for("login_2fa"))
 
